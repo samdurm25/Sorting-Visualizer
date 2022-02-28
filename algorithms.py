1e4634bcb5a40fda9e8b257532473cc911f41587
samdurm <samdurm@iu.edu>
2022-02-28 15:28:21 -0500

    creating basic outline of classes and main

---
+++ algorithms.py
@@ -0,0 +1,15 @@
+class Algorithms: 
+	def __init__(self):
+		self.
+		self.
+
+class SelectionSort(Algorithm): 
+
+class BubbleSort(Algorithm): 
+
+class InsertionSort(Algorithm): 
+
+class MergeSort(Algorithm): 
+
+class QuickSort(Algorithm): 
+
---
+++ visualizer.py
@@ -0,0 +1,24 @@
+# Imports 
+import algorithms
+import time 
+import os
+import sys
+import pygame 
+
+
+# Visualizer Window Dimensions 
+dimensions = [1024, 512]
+
+# Dictionary of all algorithms with value as execution 
+algorithms = {
+			"Selection Sort": algorithms.SelectionSort(), 
+			"Bubble Sort": algorithms.BubbleSort(), 
+			"Insertion Sort": algorithms.InsertionSort(), 
+			"Merge Sort": algorithms.MergeSort(), 
+			"Quick Sort": algorithms.QuickSort()
+			}
+
+def main():
+
+if __name__ == "__main__":
+	main()
